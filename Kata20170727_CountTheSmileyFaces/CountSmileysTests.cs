a58233e06daa99c24fa3028ff6d99f0715cb513b
cash <cash@cashwu.com>
2017-07-27 12:44:47 +0800

    add codeware test

--- Kata20170727_CountTheSmileyFaces/CountSmileysTests.cs
+++ Kata20170727_CountTheSmileyFaces/CountSmileysTests.cs
@@ -30,13 +30,37 @@ namespace Kata20170727_CountTheSmileyFaces
             var actual = kata.CountSmileys(new[] { ":)", ":D" });
             Assert.AreEqual(2, actual);
         }
+
+        [TestMethod]
+        public void input_5_smiley_should_1_smileys()
+        {
+            var kata = new Kata();
+            var actual = kata.CountSmileys(new[] { ";]", ":[", ";*", ":$", ";-D" });
+            Assert.AreEqual(1, actual);
+        }
+
+        [TestMethod]
+        public void input_4_smiley_should_3_smileys()
+        {
+            var kata = new Kata();
+            var actual = kata.CountSmileys(new[] { ":)", ";(", ";}", ":-D" });
+            Assert.AreEqual(2, actual);
+        }
+
+        [TestMethod]
+        public void input_4_smiley_should_2_smileys()
+        {
+            var kata = new Kata();
+            var actual = kata.CountSmileys(new[] { ";D", ":-(", ":-)", ";~)" });
+            Assert.AreEqual(3, actual);
+        }
     }
 
     public class Kata
     {
         public int CountSmileys(string[] smileys)
         {
-            var smileyKeys = new[] { ':', ')', 'D' };
+            var smileyKeys = new[] { ':', ';', '-', '~', ')', 'D' };
             return smileys.Count(a => a.Trim(smileyKeys).Length == 0);
         }
     }
